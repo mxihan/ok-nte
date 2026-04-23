4543967c61d3a1f6c5e1651a050b2e13fc82ee73
RU <110257560+BnanZ0@users.noreply.github.com>
2026-04-24 00:48:45 +0800

    refactor(assets): 更新 coco_annotations.json 分辨率至 1440p 并重新计算标注
    feat(esper_icons): 新增元素图标

--- src/Labels.py
+++ src/Labels.py
@@ -2,7 +2,6 @@ from enum import Enum
 
 
 class Labels(str, Enum):
-    blue_element = 'blue_element'
     boss_lv_text = 'boss_lv_text'
     box_char_1 = 'box_char_1'
     box_char_2 = 'box_char_2'
@@ -14,10 +13,5 @@ class Labels(str, Enum):
     char_2_text = 'char_2_text'
     char_3_text = 'char_3_text'
     char_4_text = 'char_4_text'
-    green_element = 'green_element'
     health_bar_slash = 'health_bar_slash'
     mini_map_arrow = 'mini_map_arrow'
-    purple_element = 'purple_element'
-    red_element = 'red_element'
-    white_element = 'white_element'
-    yellow_element = 'yellow_element'
